5e832ad3052314fc55ce6da03c2dd5402c2f7616
Playermet <playermet@gmail.com>
2015-12-22 01:11:33 +0200

    Add livr.new method;

--- livr.lua
+++ livr.lua
@@ -1,3 +1,24 @@
-local mod = {}
+local aux = {}
+
+function aux.class(class)
+  class = class or {}
+  class.__index = class
+  return class
+end
+
+function aux.object(class, object)
+  return setmetatable(object, class)
+end
+
+
+local mod = aux.class()
+
+function mod.new(class, scheme)
+  return aux.object(class, {
+    scheme = scheme;
+    rules = {};
+    short = {};
+  })
+end
 
 return mod
